8b25a056997fda7f4db6da83133841023ca19c8d
King'ori Maina <j@kingori.co>
2025-04-09 17:27:33 +0200

    Validate Exposition::BatchRequest#data to an array of hashes

--- app/models/bible/section.rb
+++ app/models/bible/section.rb
@@ -181,7 +181,7 @@ class Bible::Section < ApplicationRecord
         }
       end
 
-      batch_request.update! data: batch_data.to_json
+      batch_request.update! data: batch_data
       batch_request
     end
   end
